7ce628c4ea2b8b18dbc60c2fe7213fa24a4edd86
sudeep037 <sudeepdey037@gmail.com>
2024-01-29 16:09:47 +0530

    regression commit

---
+++ src/test/java/jenkinsPractice/class2.java
@@ -0,0 +1,12 @@
+package jenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class class2 {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Regression test executed");
+	}
+}
